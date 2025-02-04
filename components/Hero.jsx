aaf91e7ad81abde562535fbd1541762ac1117a10
jackvijayy <jackvijay1108@gmail.com>
2025-02-04 20:30:39 +0530

    update herO

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,7 +7,7 @@ import Ballpit from "./Backgrounds/Ballpit/Ballpit";
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
-      <div>
+      {/* <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
           fill="blue"
@@ -17,7 +17,7 @@ const Hero = () => {
           fill="white"
         />
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="white" />
-      </div>
+      </div> */}
       {/* grid */}
       <div className="h-screen w-full dark:bg-black   dark:bg-grid-white/[0.03] bg-grid-black/[0.3]  flex items-center justify-center absolute top-0 left-0">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_1%,black)]"></div>
